d2a921e964d762223d943d01aa750e9363665afa
Jose Castro <42446919+josecarloscas@users.noreply.github.com>
2021-04-21 14:19:55 -0500

    Add files via upload
    Esquema de los tipos de datos que se reciben

---
+++ backend/models/Product.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+let productSchema = new Schema({
+  name: {
+    type: String
+  },
+  category: {
+    type: String
+  },
+  price: {
+    type: Number
+  }
+}, {
+    collection: 'products'
+  })
+
+module.exports = mongoose.model('Product', productSchema)
